d98277ced13e40eab775e19dd87768f04524190c
François D <fderu44@gmail.com>
2023-06-03 16:29:29 +0200

    Création de la fonction playround()

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let playerChoice = "Paper";
-let computerChoice = getComputerChoice();
+const playerChoice = "Scissors";
+const computerChoice = getComputerChoice();
 
 function getComputerChoice() {
     const possibleChoice = ["Rock", "Paper", "Scissors"];
@@ -11,4 +11,20 @@ function getComputerChoice() {
     return choice;
  }
 
- 
+ function playRound(playerChoice, computerChoice) {
+    if(playerChoice === "Rock" && computerChoice === "Paper") {
+        return "You lose! Paper beats Rock"
+    } else if(playerChoice === "Paper" && computerChoice === "Scissors") {
+        return "You lose! Scissors beats Paper"
+    } else if(playerChoice === "Scissors" && computerChoice === "Rock") {
+        return "You lose! Rock beats Scissors"
+    } else if(playerChoice === "Rock" && computerChoice === "Scissors") {
+        return "You win! Rock beats Scissors"
+    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        return "You win! Scissors beats Rock"
+    } else if(playerChoice === "Paper" && computerChoice === "Rock") {
+        return "You win! Paper beats Rock"
+    }
+ }
+
+ console.log(playRound(playerChoice, computerChoice));
