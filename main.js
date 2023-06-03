fe9878b15535dca40fc42480c362e01dcb9d8f74
François D <fderu44@gmail.com>
2023-06-03 16:07:34 +0200

    Correction de la fonction getComputerChoice()

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-let playerChoice = "Rock";
+let playerChoice = "Paper";
 let computerChoice = getComputerChoice();
 
 function getComputerChoice() {
-    const possibleChoice = ["Rock", "Paper", "Scissor"];
+    const possibleChoice = ["Rock", "Paper", "Scissors"];
     let choice = possibleChoice[Math.floor(Math.random() * 3)];
+
+    if(choice === playerChoice) {
+        choice = getComputerChoice();
+    }
     return choice;
  }
+
+ 
