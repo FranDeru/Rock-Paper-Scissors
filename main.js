a56ed7b0ee5a510dfe0da76e71e89898b6a60769
François D <fderu44@gmail.com>
2023-06-27 15:38:05 +0200

    Création de la partie ui du site

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 let playerChoice;
 let computerChoice;
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('#result');
+const score = document.querySelector('#score');
 
-function initializeRound() {
-    playerScore = 0;
-    computerScore = 0;
-}
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playerChoice = button.value;
+        computerChoice = getComputerChoice().toLowerCase();
+
+        playRound(playerChoice, computerChoice);
+        score.textContent = `Player: ${playerScore}   Computer: ${computerScore}`;
+
+        if(playerScore === 5 || computerScore === 5){
+            declareWinner();
+        }
+    });
+});
 
 function getComputerChoice() {
     const possibleChoice = ["Rock", "Paper", "Scissors"];
@@ -16,34 +28,42 @@ function getComputerChoice() {
 
  function playRound(playerChoice, computerChoice) {
     if(playerChoice === computerChoice) {
-        return "It's a tie!"
+        result.textContent = "Its a tie!"
     }else if((playerChoice === "rock" && computerChoice === "paper") ||
         (playerChoice === "paper" && computerChoice === "scissors") ||
         (playerChoice === "scissors" && computerChoice === "rock")) {
         computerScore = ++computerScore;
-        return "You lose! Paper beats Rock" 
+        result.textContent = `You lose, ${computerChoice} beats the ${playerChoice}`;
     } else{
         playerScore = ++playerScore;
-        return "You win! Paper beats Rock"
+        result.textContent = `You win, ${playerChoice} beats the ${computerChoice}`;
     }
  }
 
- function game() {
-    initializeRound();
-    for(let i = 0; i < 5; i++){
-        playerChoice = prompt("Choose your weapons", "Rock").toLowerCase();
-        computerChoice = getComputerChoice().toLowerCase();
-        playRound(playerChoice, computerChoice);
-        console.log(playerScore, computerScore);
-    }
-
-    if(playerScore == computerScore) {
-        alert("No winner for this time");
-    } else if(playerScore > computerScore){
-        alert("You win the game!");
-    } else{
-        alert("You lose the game!");
-    }
+ function declareWinner() {
+    if(playerScore === 5) {
+        alert('You won the game!');
+    } else {
+        alert('You lose the game!');
+    };
+    playerScore = 0;
+    computerScore= 0;
  }
 
- console.log(game());
+//  function game() {
+//     initializeRound();
+//     for(let i = 0; i < 5; i++){
+//         playerChoice = prompt("Choose your weapons", "Rock").toLowerCase();
+//         computerChoice = getComputerChoice().toLowerCase();
+//         playRound(playerChoice, computerChoice);
+//         console.log(playerScore, computerScore);
+//     }
+
+//     if(playerScore == computerScore) {
+//         alert("No winner for this time");
+//     } else if(playerScore > computerScore){
+//         alert("You win the game!");
+//     } else{
+//         alert("You lose the game!");
+//     }
+//  }
